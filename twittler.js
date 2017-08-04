80b0e4a51e6d0b0c5339d988dddb1d667de8911a
Dominic Ma <dominic.c.ma@gmail.com>
2017-08-04 01:25:16 -0700

    "Added ability to view a user's timeline by clicking username"

--- twittler.js
+++ twittler.js
@@ -2,37 +2,60 @@
 
 $(document).ready(function(){
   // Generates and displays initial Tweets
-  updateTweets();
+  display.updateTweets();
 
   // Show New Tweets Button
   $('.tweet-update').on('click', 'button', function() {
-    updateTweets();
+    display.updateTweets();
   });
+  
+  // Shows user's timeline when username is clicked
+  $('.filterByUser').on('click', function() {
+    display.tweetsByUser($(this).text());
+  })
 
 
 });
 
 /* Specs: 
 1. Be able to show the user new tweets. Either automatically as they are created, or with a button X
-2. Display Timestamps of new created tweets
+2. Display Timestamps of new created tweets X
 3. Design the Interface
-4. Clicking on a username shows that user's timeline.
+4. Clicking on a username shows that user's timeline. X
 */
 
 
-// SPEC 1: SHOW NEW TWEETS WITH A BUTTON
-// Updates the DOM with new tweets
 
-var updateTweets = function() {
+var display = {
+  updateTweets : function() {
     var $body = $('.tweet-log');
     $body.html('');
     var index = streams.home.length - 1;
     while(index >= 0){
       var tweet = streams.home[index];
-      var $tweet = $('<div></div>');
-      $tweet.text('@' + tweet.user + ': ' + tweet.message);
+      var $tweet = $('<div>' + '['+ tweet.created_at + '] ' + '@' + '<a href=# class="filterByUser">' + tweet.user + '</a>' + ': ' + tweet.message + '</div>' );
+      
       $tweet.appendTo($body);
       index -= 1;
     }
-  }; 
+    this.resetUserFilter();
+  },
+  tweetsByUser : function(user) {
+    var $body = $('.tweet-log');
+    $body.html('');
+    var index = streams.users[user].length - 1;
+    while(index >= 0){
+      var tweet = streams.users[user][index];
+      var $tweet = $('<div>' + '['+ tweet.created_at + '] ' + '@' + '<a href=# class="filterByUser">' + tweet.user + '</a>' + ': ' + tweet.message + '</div>');
+      $tweet.appendTo($body);
+      index -= 1;
+    }
+    this.resetUserFilter();
+  },
+  resetUserFilter : function() {
+    $('.filterByUser').on('click', function() {
+    display.tweetsByUser($(this).text());
+    })
+  }
+} 
 
